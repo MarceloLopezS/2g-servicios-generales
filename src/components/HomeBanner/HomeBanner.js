843ec9f777c6f7c700aec47e32486a9b7e6f8276
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2022-09-21 16:39:27 -0500

    Optimize CSS

--- src/components/HomeBanner/HomeBanner.js
+++ src/components/HomeBanner/HomeBanner.js
@@ -14,14 +14,14 @@ const HomeBanner = () => {
                     <img src={logoPath} alt='2G Logo'></img>
                 </div>
                 <div className='banner__body'>
-                    <h1 className='text-primary-400'>Diseño <span className='text-accent-100'>&</span> Construcción</h1>
+                    <h1 className='highlighted fs-banner-title text-primary-400 padding-block-100'>Diseño <span className='text-accent-100'>&</span> Construcción</h1>
                     <p>Somos una empresa con una década de experiencia en materializar los proyectos de nuestros clientes siguiendo los más altos estándares de calidad, seguridad y medio ambiente.</p>
                 </div>
-                <div className='banner__scroll-wraper'>
+                <div className='banner__scroll-wraper | padding-top-200'>
                     <div className='banner__scroll-icon-rail'>
                         <div className='banner__scroll-icon-moving'></div>
                     </div>
-                    <span><a href='#'>Explora más</a></span>
+                    <span className='highlighted'><a href='#'>Explora más</a></span>
                 </div>
             </div>
         </section>

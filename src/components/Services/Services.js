02d3a4783208d2b50041218747411b35a2df0227
Marcelo Lopez Silva <marcelo-lo@outlook.com>
2022-11-24 19:59:47 -0500

    Update services order

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Services.css';
 import serviceImg1 from '../../assets/images/levantamiento-topografico.webp';
-import serviceImg2 from '../../assets/images/movimiento-tierras.webp';
-import serviceImg3 from '../../assets/images/saneamiento-fisico-legal-predios.webp';
+import serviceImg2 from '../../assets/images/construccion-civil.webp';
+import serviceImg3 from '../../assets/images/movimiento-tierras.webp';
 import serviceImg4 from '../../assets/images/sistemas-drenaje.webp';
 import serviceImg5 from '../../assets/images/evaluacion-sistemas-bombeo.webp';
-import serviceImg6 from '../../assets/images/construccion-civil.webp';
+import serviceImg6 from '../../assets/images/saneamiento-fisico-legal-predios.webp';
 
 const Services = ({ reference }) => {
     return (
@@ -33,20 +33,20 @@ const Services = ({ reference }) => {
                 </div>
                 <div className='card' data-section='services'>
                     <div className='card__media'>
-                        <img loading='lazy' src={serviceImg2} alt='Movimiento de tierras'></img>
+                        <img loading='lazy' src={serviceImg2} alt='Construcción Civil'></img>
                     </div>
                     <div className='card__body'>
-                        <h3>Movimiento de tierras</h3>
-                        <p>Ejecutamos con eficacia proyectos de calidad, gracias a nuestro personal, equipos y maquinaria.</p>
+                        <h3>Construcción Civil</h3>
+                        <p>Ofrecemos planificación, cimenta&shy;ción, edificación, instalación de tuberías, electricidad y remodelación.</p>
                     </div>
                 </div>
                 <div className='card' data-section='services'>
                     <div className='card__media'>
-                        <img loading='lazy' src={serviceImg3} alt='Saneamiento físico legal de predios'></img>
+                        <img loading='lazy' src={serviceImg3} alt='Movimiento de tierras'></img>
                     </div>
                     <div className='card__body'>
-                        <h3>Saneamiento físico legal de predios</h3>
-                        <p>Brindamos asesoría técnico legal en todo el proceso de saneamiento, asegurando su inversión.</p>
+                        <h3>Movimiento de tierras</h3>
+                        <p>Ejecutamos con eficacia proyectos de calidad, gracias a nuestro personal, equipos y maquinaria.</p>
                     </div>
                 </div>
                 <div className='card' data-section='services'>
@@ -69,11 +69,11 @@ const Services = ({ reference }) => {
                 </div>
                 <div className='card' data-section='services'>
                     <div className='card__media'>
-                        <img loading='lazy' src={serviceImg6} alt='Construcción Civil'></img>
+                        <img loading='lazy' src={serviceImg6} alt='Saneamiento físico legal de predios'></img>
                     </div>
                     <div className='card__body'>
-                        <h3>Construcción Civil</h3>
-                        <p>Ofrecemos planificación, cimenta&shy;ción, edificación, instalación de tuberías, electricidad y remodelación.</p>
+                        <h3>Saneamiento físico legal de predios</h3>
+                        <p>Brindamos asesoría técnico legal en todo el proceso de saneamiento, asegurando su inversión.</p>
                     </div>
                 </div>
             </section>
